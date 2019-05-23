21b1e91c457df397c9e60f2cfb6ee6c7627e3e55
Raul Fernando Casallas Malaver <rfcasallas@gmail.com>
2019-05-23 03:10:49 -0500

    Primer commit

---
+++ src/co/example/second/OtherClass.java
@@ -0,0 +1,15 @@
+package co.example.second;
+
+public class OtherClass extends SimpleClass {
+
+	private String otroAtributo;
+
+	public String getOtroAtributo() {
+		return otroAtributo;
+	}
+
+	public void setOtroAtributo(String otroAtributo) {
+		this.otroAtributo = otroAtributo;
+	}	
+	
+}
---
+++ src/co/example/second/SimpleClass.java
@@ -0,0 +1,45 @@
+package co.example.second;
+
+public class SimpleClass {
+
+	private int id;
+	
+	private String name;
+	
+	private double doubleValue;
+	
+	private long longValue;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getDoubleValue() {
+		return doubleValue;
+	}
+
+	public void setDoubleValue(double doubleValue) {
+		this.doubleValue = doubleValue;
+	}
+
+	public long getLongValue() {
+		return longValue;
+	}
+
+	public void setLongValue(long longValue) {
+		this.longValue = longValue;
+	}	
+	
+}
